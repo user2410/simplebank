7f383ba2e8d418181991bb5697165a5db9128f2b
user2410 <dc241024@gmail.com>
2022-09-26 17:30:38 +0700

    load secrets to app.env file

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# This script is expected to be executed inside container
+
+# Exit immediately if a simple command exits with a non-zero status
 set -e
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "run the app"
+# Example: entrypoint.sh server start -> server start
 exec "$@"
