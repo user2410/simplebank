7a484408fc1e95da4de20c742e2a275364bc0b07
user2410 <dc241024@gmail.com>
2022-09-26 22:53:22 +0700

    fix env set execute permission

--- start.sh
+++ start.sh
@@ -5,10 +5,6 @@
 # Exit immediately if a simple command exits with a non-zero status
 set -e
 
-# Set execute permission for shell scripts
-chmod +x /app/start.sh
-chmod +x /app/wait-for.sh
-
 echo "run db migration"
 source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
